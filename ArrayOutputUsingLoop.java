25d08232dcee7da3cb87175690c06e2abf295dd5
wasiullah0419 <wasishah0419@gmail.com>
2026-03-29 22:40:14 -1200

    Aray program Output array element using loop

---
+++ ArrayOutputUsingLoop.java
@@ -0,0 +1,11 @@
+class ArrayOutputUsingLoop {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        System.out.println("Array elements using for loop:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    
+    }
+}
