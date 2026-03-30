cd47146b2b61d2542318f11ce1de34566f1b4f32
wasiullah0419 <wasishah0419@gmail.com>
2026-03-30 00:25:57 -1200

    write a code in java to print negative elements of array element

---
+++ NegativeArrayElementPrint.java
@@ -0,0 +1,21 @@
+ 
+ // write a code in java to print negative of array element
+ import java.util.Scanner;
+ class NegativeArrayElementPrint {
+    public static void  main(String args[])
+    {
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enater your array length");
+          int n=sc.nextInt();
+          int []arr= new int [n];
+           System.out.println("Enater your array element");
+          for(int i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+        System.out.println("print only negative element");
+        for(int i=0;i<n;i++){
+             if (arr[i]<0) 
+                System.out.println(arr[i]);
+        }
+       
+    }
+}
